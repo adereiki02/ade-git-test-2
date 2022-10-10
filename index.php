bf5cc6994939a3d45f3a6fddba5c374c85bd2702
adereiki02 <adereiki02@gmail.com>
2022-10-10 13:48:12 +0700

    Menambahkan list pada file index.php

--- index.php
+++ index.php
@@ -9,5 +9,9 @@
 <body>
     <h1>Hello World</h1>
     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Esse laboriosam ipsa dicta, a quae eos dignissimos eligendi, quidem deserunt tempore dolorum earum architecto ex neque ab quibusdam necessitatibus, eius quos!</p>
+    <ul>
+        <li>satu</li>
+        <li>dua</li>
+    </ul>
 </body>
 </html>
